7857e83cb5178df12f4b366e195d1c6b75845b59
Andrea Jessen <andrea_jes@hotmail.com>
2023-03-13 14:29:33 +0100

    Add functionality for moving camera

--- src/datatypes/virtualCamera.py
+++ src/datatypes/virtualCamera.py
@@ -3,17 +3,24 @@ from datatypes.projectedPoint import ProjectedPoint
 
 class VirtualCamera:
     
-    def __init__(self, position_WRF, roll, yaw, pitch, focal_length, p_x, p_y, principal_point, image_bounds, skew=0):
+    def __init__(self, focal_length, p_x, p_y, principal_point, image_bounds, skew=0):
         '''
         Input:
         principal_point: Principal point of image plane (c_x,c_y) in pixel coordinates
         '''
         self.image_bounds = image_bounds # In pixels (x,y)
-        # Position in WRF coordinates
-        self.position_WRF = position_WRF
-        self.roll = roll # Roll is a counterclockwise rotation of φ about the x-axis
-        self.yaw = yaw # The yaw is a counterclockwise rotation of θ about the z-axis. 
-        self.pitch = pitch # The pitch is a counterclockwise rotation of ψ about the y-axis.
+        # Position and orientation in VCF coordinates
+        # Initialized as 0, changed if the camera is placed on a vessel (self.place_camera_on_vessel)
+        self.position_vcf = np.array([0,0,0])
+        self.roll_vcf = 0
+        self.pitch_vcf = 0
+        self.yaw_vcf = 0
+        # Position in WCF coordinates
+        # If the camera is placed on a vessel, these parameters are updated for each timestep. It is the CameraRig that knows the track of the camera. 
+        self.position_wcf = np.array([0,0,0])
+        self.roll_wcf = 0
+        self.pitch_wcf = 0
+        self.yaw_wcf = 0
 
         # Intrinsic parameters
         self.focal_length = focal_length
@@ -30,35 +37,49 @@ class VirtualCamera:
 
 
         self.K = np.array([[self.f_x, self.skew, self.c_x], [0, self.f_y, self.c_y], [0, 0, 1]])
+        # self.calculate_projection_matrix()
+
+    def place_camera_on_vessel(self, position_vcf, roll_vcf, pitch_vcf, yaw_vcf):
+        self.position_vcf = position_vcf
+        self.roll_vcf = roll_vcf
+        self.pitch_vcf = pitch_vcf
+        self.yaw_vcf = yaw_vcf
+    
+    def place_camera_in_world(self, position_wcf, roll_wcf, pitch_wcf, yaw_wcf):
+        self.position_wcf = position_wcf
+        self.roll_wcf = roll_wcf
+        self.pitch_wcf = pitch_wcf
+        self.yaw_wcf = yaw_wcf
         self.calculate_projection_matrix()
 
-    
+    def update_camera_world_pos(self, position_wcf, yaw_wcf, vcf_wcf_rotation_matrix):
+        self.position_wcf = vcf_wcf_rotation_matrix.dot(self.position_vcf) + position_wcf
+        self.yaw_wcf = self.yaw_vcf + yaw_wcf
+        self.calculate_projection_matrix()
+
     def get_intrinsic_camera_matrix(self):
         return self.K
     
-    def get_position(self):
-        return self.position_WRF
+    def get_position_vcf(self):
+        return self.position_vcf
+    
+    def get_position_wcf(self):
+        return self.position_wcf
     
     def calculate_rotation_matrix(self):
-        R_roll = np.round(np.array([[np.cos(self.roll), -np.sin(self.roll), 0], [np.sin(self.roll), np.cos(self.roll), 0], [0,0,1]]),5)
-        R_yaw = np.round(np.array([[np.cos(self.yaw), 0, np.sin(self.yaw)],[0,1,0],[-np.sin(self.yaw), 0, np.cos(self.yaw)]]),5)
-        R_pitch = np.round(np.array([[1, 0, 0], [0, np.cos(self.pitch), -np.sin(self.pitch)], [0, np.sin(self.pitch), np.cos(self.pitch)]]),5)
+        R_roll = np.round(np.array([[np.cos(self.roll_wcf + self.roll_vcf), -np.sin(self.roll_wcf + self.roll_vcf), 0], [np.sin(self.roll_wcf + self.roll_vcf), np.cos(self.roll_wcf + self.roll_vcf), 0], [0,0,1]]),5)
+        R_yaw = np.round(np.array([[np.cos(self.yaw_wcf + self.yaw_vcf), 0, np.sin(self.yaw_wcf + self.yaw_vcf)],[0,1,0],[-np.sin(self.yaw_wcf + self.yaw_vcf), 0, np.cos(self.yaw_wcf + self.yaw_vcf)]]),5)
+        R_pitch = np.round(np.array([[1, 0, 0], [0, np.cos(self.pitch_wcf + self.pitch_vcf), -np.sin(self.pitch_wcf + self.pitch_vcf)], [0, np.sin(self.pitch_wcf + self.pitch_vcf), np.cos(self.pitch_wcf + self.pitch_vcf)]]),5)
         # np.round to 5 decimals because the number π cannot be represented exactly as a floating-point number.
         R_orientation = R_roll.dot(R_pitch.dot(R_yaw))
 
         R_align = np.array([[0,-1,0],[0, 0, -1], [1,0,0]])
         rotation_matrix = R_orientation.dot(R_align)
         return rotation_matrix
-
-    def update_camera_angle(self, roll, yaw, pitch):
-        self.roll = roll # Roll is a counterclockwise rotation of φ about the x-axis
-        self.yaw = yaw # The yaw is a counterclockwise rotation of θ about the z-axis. 
-        self.pitch = pitch # The pitch is a counterclockwise rotation of ψ about the y-axis.
-        self.calculate_projection_matrix()
     
     def calculate_projection_matrix(self):
         self.R = self.calculate_rotation_matrix()
-        t = self.R.dot(-self.position_WRF)
+        t = self.R.dot(-self.position_wcf)
         self.M = np.hstack((self.R, t[:, None]))
         self.P = self.K.dot(self.M)
 
@@ -76,11 +97,11 @@ class VirtualCamera:
         return projected_point
 
     def project_points(self, points):
-        projected_points = [self.project_point(point) for point in points if self.project_point(point).depth >=0]
+        projected_points = [self.project_point(point) for point in points]
         return np.array(projected_points)
     
     def get_orientation_vector(self):
-        y = round(np.cos(np.pi/2-self.yaw),5)
-        x = round(np.cos(self.yaw),5)
+        y = round(np.cos(np.pi/2-(self.yaw_wcf + self.yaw_vcf)),5)
+        x = round(np.cos(self.yaw_wcf + self.yaw_vcf),5)
         return np.array([x,y])
     
