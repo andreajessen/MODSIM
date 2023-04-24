d5efdbaf5763861dd1e3ba199233ca7b01ef099e
solveijm <solveig.mohr@gmail.com>
2023-04-24 09:53:05 +0200

    :sparkles: introduce clutter

--- src/errorGenerator.py
+++ src/errorGenerator.py
@@ -33,19 +33,24 @@ class ErrorGenerator:
         # Check if there is a general confusion matrix (without labels)
         if data_loaded.get('confusionMatrix'):
             self.drop_out = data_loaded['confusionMatrix']['FN']
+            self.false_positives = data_loaded['confusionMatrix']['FP']
 
         # If classification is True, get label specific confusion matrix
         if self.classification:
+            self.BACKGROUND = 'Background'
             self.possible_labels = data_loaded['labels'] # Possible classification labels
             # Confusion matrix, background not included
             self.confusion_matrix_labels = data_loaded['confusionMatrixLabels']
             self.calculate_drop_out_labels()
+            self.false_positives_class = data_loaded.get('falsePositives')
+            # Add probability for Not false positives
+            self.false_positives_class.append(max(0,1-sum(self.false_positives_class)))
 
         self.confidence_threshold = data_loaded['confidenceThreshold']
     
     
     def calculate_drop_out_labels(self):
-        self.possible_labels.append('Background')
+        self.possible_labels.append(self.BACKGROUND)
         for label, values in self.confusion_matrix_labels.items():
             drop_out = 1 - sum(values)
             self.confusion_matrix_labels[label].append(drop_out)
@@ -96,7 +101,7 @@ class ErrorGenerator:
 
     def generate_error_class(self, annot):
         label = self.generate_error_label(annot.label)
-        if label == 'Background':
+        if label == self.BACKGROUND:
             return None
         eBB = self.generate_error_BB(annot)
         confidence_score = self.generate_confidence_score()
@@ -117,8 +122,51 @@ class ErrorGenerator:
         return self.generate_error_detection(annot)
 
     
-    def generate_detections_t(self, annots_t, t, writeToJson=False, folder_path=None):
+    def generate_detections_t(self, annots_t, t, image_bounds, horizon, writeToJson=False, folder_path=None):
         detections = list(filter(lambda item: item is not None, [self.generate_error(annot) for annot in annots_t]))
+        false_detections = self.generate_false_positives(image_bounds, horizon, len(detections))
+        if false_detections:
+            detections.extend(false_detections)
         if writeToJson and folder_path:
             update_detections_json(detections, folder_path, t)
         return detections
+    
+
+    def create_random_detection(self, image_bounds, horizon, label):
+        cx = np.random.uniform(0, image_bounds[0])
+        cy = np.random.uniform(int(horizon[0].get_y()), image_bounds[1])
+        h = 100 # What should the size be?
+        w = 100 # What should the size be?
+        bb = BoundingBox(None, [cx, cy], w, h, None)
+        confidence_score = self.confidence_threshold # How to set this?
+    
+        return Detection(bb, label, None, confidence_score)
+
+    def generate_false_positives(self, image_bounds, horizon, numb_detections):
+        total_fp = sum(self.false_positives_class[:-1]) if self.classification else self.false_positives
+        # Higher confidence threshold should give less clutter
+        # The maximum number of false positives are dependent on how many detections there are in the image
+        # to ensure that the number of false positive bounding boxes is balanced with the number of true positive bounding boxes,
+        # Confidence threshold should influence the amount of clutter
+        max_fp = max(0, round(total_fp*numb_detections-self.confidence_threshold))
+        if max_fp == 0: return None
+        if self.classification:
+            false_detections = []
+            for _ in range(max_fp):
+                label = np.random.choice(self.possible_labels, p = self.false_positives_class)
+                if label!=self.BACKGROUND: 
+                    detection = self.create_random_detection(image_bounds, horizon, label)
+                    false_detections.append(detection)
+        else:
+            false_detections = []
+            for _ in range(max_fp):
+                is_fp = np.random.choice([True, False], p=[self.false_positives, 1-self.false_positives])
+                if is_fp:
+                    detection = self.create_random_detection(image_bounds, horizon, None)
+                    false_detections.append(detection)
+        return false_detections
+
+
+
+        
+
