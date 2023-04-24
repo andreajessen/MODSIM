13459ca939f2b0b5046c29fcfaf5cd2b1159e460
solveijm <solveig.mohr@gmail.com>
2023-04-24 08:25:58 +0200

    :sparkles: drop out based on class

--- src/errorGenerator.py
+++ src/errorGenerator.py
@@ -1,3 +1,4 @@
+from datatypes.annotation import Annotation
 from datatypes.boundingBox import BoundingBox
 from datatypes.detection import Detection
 import numpy as np 
@@ -13,7 +14,6 @@ class ErrorGenerator:
             data_loaded = yaml.safe_load(stream)
 
         # How to get this in the best way
-        self.drop_out = data_loaded['confusionMatrix']['FN']
         self.sigma_cx = data_loaded['errorStats']['sigma_cx'] # Standard deviation
         self.mu_cx = data_loaded['errorStats']['mu_cx'] # Expected value
 
@@ -26,21 +26,38 @@ class ErrorGenerator:
         self.sigma_w = data_loaded['errorStats']['sigma_w'] # Standard deviation
         self.mu_w = data_loaded['errorStats']['mu_w'] # Expected value
 
-        self.possible_labels = data_loaded['labels'] #Possible classification labels
-        self.confusion_matrix_labels = data_loaded['confusionMatrixLabels']
+
+        # Check if classification should be active
+        self.classification = True if data_loaded.get('labels') else False
+
+        # Check if there is a general confusion matrix (without labels)
+        if data_loaded.get('confusionMatrix'):
+            self.drop_out = data_loaded['confusionMatrix']['FN']
+
+        # If classification is True, get label specific confusion matrix
+        if self.classification:
+            self.possible_labels = data_loaded['labels'] # Possible classification labels
+            # Confusion matrix, background not included
+            self.confusion_matrix_labels = data_loaded['confusionMatrixLabels']
+            self.calculate_drop_out_labels()
 
         self.confidence_threshold = data_loaded['confidenceThreshold']
     
+    
+    def calculate_drop_out_labels(self):
+        self.possible_labels.append('Background')
+        for label, values in self.confusion_matrix_labels.items():
+            drop_out = 1 - sum(values)
+            self.confusion_matrix_labels[label].append(drop_out)
+            
 
-    def generate_error_BB(self, BB: BoundingBox):
+
+    def generate_error_BB(self, annot: Annotation):
         '''
         Input: 
-        boundingBox (BoundingBox)
+        annot (Annotation)
         '''
-        if self.is_dropout():
-            # Return no BB, because dropout happened
-            return None
-        
+        BB = annot.bb
         # Introduce error based on normal distribution
         e_cx = float(np.random.normal(self.mu_cx, self.sigma_cx, 1))
         e_cy = float(np.random.normal(self.mu_cy, self.sigma_cy, 1))
@@ -70,17 +87,34 @@ class ErrorGenerator:
         return confidence_score
 
     def is_dropout(self):
-        # Should BB size affect dropout
-        dropout = np.random.choice([True, False], p=[self.drop_out, 1-self.drop_out])
-        return dropout
+        '''
+        Calculates dropout when drop out rate is not dependent on classification
+        '''
+        # Should BB distance affect dropout
+        is_drop_out = np.random.choice([True, False], p=[self.drop_out, 1-self.drop_out])
+        return is_drop_out
 
-    
-    def generate_error(self, annot):
-        eBB = self.generate_error_BB(annot.bb)
-        if not eBB: return None
+    def generate_error_class(self, annot):
         label = self.generate_error_label(annot.label)
+        if label == 'Background':
+            return None
+        eBB = self.generate_error_BB(annot)
         confidence_score = self.generate_confidence_score()
         return Detection(eBB, label, annot.vesselID, confidence_score)
+    
+    def generate_error_detection(self, annot):
+        label = None
+        if self.is_dropout():
+            return None
+        eBB = self.generate_error_BB(annot)
+        confidence_score = self.generate_confidence_score()
+        return Detection(eBB, label, annot.vesselID, confidence_score)
+
+        
+    def generate_error(self, annot: Annotation):
+        if self.classification:
+            return self.generate_error_class(annot)
+        return self.generate_error_detection(annot)
 
     
     def generate_detections_t(self, annots_t, t, writeToJson=False, folder_path=None):
