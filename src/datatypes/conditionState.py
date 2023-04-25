31fa798125ba36fcdc9ebb476d4ed81d52621ac6
solveijm <solveig.mohr@gmail.com>
2023-04-25 15:29:14 +0200

    :sparkles: temporal model with classification

--- src/datatypes/conditionState.py
+++ src/datatypes/conditionState.py
@@ -1,6 +1,6 @@
 class ConditionState:
 
-    def __init__(self, name, id, confusion_matrix):
+    def __init__(self, name, id, confusion_matrix, confusion_matrix_labels=None):
         '''
         Input:
         - Name (String)
@@ -10,6 +10,14 @@ class ConditionState:
         self.name = name
         self.id = id
         self.confusion_matrix = confusion_matrix
+        self.confusion_matrix_labels = confusion_matrix_labels
+
 
     def get_dropout(self):
         return self.confusion_matrix['FN']
+    
+    def get_false_positives(self):
+        return self.confusion_matrix['FP']
+    
+    def set_confusion_matrix_labels(self, confusion_matrix_labels):
+        self.confusion_matrix_labels = confusion_matrix_labels
