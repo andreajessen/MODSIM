701365a0cd81cdb9b67ca35340d56e20e6acb3e2
solveijm <solveig.mohr@gmail.com>
2023-05-26 21:28:40 +0200

    run simulation 1

--- src/datatypes/conditionState.py
+++ src/datatypes/conditionState.py
@@ -1,23 +1,45 @@
+from dataclasses import dataclass
+
+@dataclass
+class BBStats:
+    sigma_cx: float
+    mu_cx: float
+    sigma_cy: float
+    mu_cy: float
+    sigma_h: float
+    mu_h: float
+    sigma_w: float
+    mu_w: float
+
 class ConditionState:
 
-    def __init__(self, name, id, confusion_matrix, confusion_matrix_labels=None):
+    def __init__(self, name, id, confusion_matrix, bb_stats, fdr):
         '''
         Input:
         - Name (String)
         - Id (int)
-        - Confusion matrix: {'FN': 0.2, 'FP': 0.2, 'TP': 0.8, 'TN': 0.8} 
+        - Confusion matrix: {'Label': [List], 'Label': [List]}. Ex: {'Vessel': [0.867, 0.133], 'Background': [1, None]}
+        - fdr (float): False discovery rate
         '''
         self.name = name
         self.id = id
         self.confusion_matrix = confusion_matrix
-        self.confusion_matrix_labels = confusion_matrix_labels
+        self.bb_stats = BBStats(
+            sigma_cx=bb_stats['sigma_cx'], 
+            mu_cx=bb_stats['mu_cx'], 
+            sigma_cy=bb_stats['sigma_cy'], 
+            mu_cy=bb_stats['mu_cy'], 
+            sigma_h=bb_stats['sigma_h'],
+            mu_h=bb_stats['mu_h'],
+            sigma_w=bb_stats['sigma_w'],
+            mu_w=bb_stats['mu_w']
+        )
+        self.fdr = fdr
 
+    def get_dropout(self, label):
+        return self.confusion_matrix[label][-1]
+    
+    def get_false_discovery_rate(self):
+        return self.fdr
 
-    def get_dropout(self):
-        return self.confusion_matrix['FN']
     
-    def get_false_positives(self):
-        return self.confusion_matrix['FP']
-    
-    def set_confusion_matrix_labels(self, confusion_matrix_labels):
-        self.confusion_matrix_labels = confusion_matrix_labels
